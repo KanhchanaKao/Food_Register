18b44e9641861207415e457a21cd5ddfb314dfa8
KanhchanaKao <kanhchana111@gmail.com>
2022-02-22 11:08:15 +0700

    login update

---
+++ lib/Login/loginPage.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import 'header.dart';
+import 'inputWrapper.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              colors: [Colors.green[700]]),
+        ),
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 80,
+            ),
+            Header(),
+            Expanded(
+                child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50),
+                    topRight: Radius.circular(50),
+                  )),
+              child: InputWrapper(),
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
